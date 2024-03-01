f1b1660f11e17b4a99bfef327857ff98ff59752b
amoiiiiii <gurlsweetie6@gmail.com>
2024-03-01 14:35:34 +0700

    ujikombosh

---
+++ src/views/Banner.vue
@@ -0,0 +1,241 @@
+<template>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;1,600&display=swap" rel="stylesheet" />
+
+<div class="w-screen bg-pink-50">
+
+<div class="relative mx-auto flex flex-col px-4 sm:max-w-xl md:max-w-screen-xl md:flex-row">
+  <!-- Left Column -->
+  <div class="mx-auto mt-10 w-full max-w-xl md:mt-36 lg:max-w-screen-xl">
+    <div>
+            <p class="bg-teal-accent-400 mb-2 inline-block rounded-full bg-pink-300 px-3 py-1 text-xs font-semibold uppercase tracking-wider text-indigo-900">Dapatkan Diskon 50% di bulan Agustus ini</p>
+          </div>
+    <div class="mb-16 lg:mb-0 lg:max-w-lg">
+      <div class="mb-6 max-w-xl">
+        <h2 class="mb-6 max-w-lg text-3xl font-bold tracking-tight text-slate-700 sm:text-5xl sm:leading-snug">
+        Selamat datang di<br />
+          <span class="inline-block font-bold text-pink-600">Azarine Collections</span>
+        </h2>
+        <p class="text-base text-black-700 md:text-lg">
+          Produk ini mengeluarkan banyak jenis perawatan kecantikan seperti  : 
+          <br>
+         makeup, skincare, dan body care</p>
+      </div>
+      <div class="flex items-center">
+        <a href="/produk" class="mr-6 inline-flex h-12 items-center justify-center rounded bg-pink-800 px-8 font-medium tracking-wide text-white shadow-md outline-none transition duration-200 hover:bg-pink-400 focus:ring"> Shop Now </a>
+      </div>
+    </div>
+  </div>
+  <!-- /Left Column -->
+
+  <!-- Right Column -->
+  <div class="flex h-full w-full space-x-5 overflow-hidden md:justify-end lg:px-6">
+      <!-- Col 2 -->
+      <div class="w-full flex-col space-y-3 rounded-xl py-4 lg:flex lg:w-80">
+        <div class="h-40 overflow-hidden rounded-xl bg-green-600/60">
+          <img class="mx-auto h-full w-full object-cover" src="https://images.tokopedia.net/img/cache/700/VqbcmM/2023/11/30/15dd4b71-f0f1-4b8d-bd0e-5c4fa6e35f4d.jpg" alt="" />
+        </div>
+        <div class="h-40 overflow-hidden rounded-xl bg-pink-600/60">
+          <img class="mx-auto h-full w-full object-cover" src="https://down-id.img.susercontent.com/file/5803cb15dcb35f64ec8b078f3a5faf6e" alt="" />
+        </div>
+        <div class="h-40 overflow-hidden rounded-xl bg-blue-600/60">
+          <img class="mx-auto h-full w-full object-cover" src="https://images.tokopedia.net/img/cache/500-square/hDjmkQ/2023/1/26/f0d1a4b8-d4f0-46a9-b737-d9c09d9004ca.jpg" alt="" />
+        </div>
+      </div>
+  <!-- /Right Column -->
+</div>
+</div>
+</div>
+<br>
+<br>
+<div class="mx-auto my-5 w-screen max-w-screen-xl px-4">
+  <div class="flex rounded-xl bg-white py-2 px-2 text-gray-700 shadow-lg shadow-pink-200">
+    <div class="my-auto mr-4 p-3 text-black-500">
+      <h1 class="text-2xl font-bold">Bodycare</h1>
+      <p class="mt-4 text-lg"> Berbagai koleksi body care kami seperti body serum & scrub dapat digunakan sebagai perawatan tubuh agar kulit lebih halus dan mengangkat sel kulit mati.</p>
+      <button type="submit" class="group mt-4 mr-2 mb-4 flex cursor-pointer text-left text-xl font-semibold leading-tight text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-7 w-7 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+        </svg>
+      </button>
+    </div>
+    <div class="hidden h-72 w-1/2 flex-shrink-0 justify-center lg:flex">
+      <img class="" src="https://www.florentbeauty.com/wp-content/uploads/2022/09/Desain-tanpa-judul-1.png" alt="" />
+    </div>
+  </div>
+</div>
+<br>
+<div class="mx-auto my-5 w-screen max-w-screen-xl px-4">
+  <div class="flex rounded-xl bg-white py-2 px-2 text-gray-700 shadow-lg shadow-pink-200">
+    <div class="my-auto mr-4 p-3 text-black-500">
+      <h1 class="text-2xl font-bold">Lip Azarine</h1>
+      <p class="mt-4 text-lg"> Lip tint ringan dengan formula tahan lama, juga diperkaya dengan Vitamin E dan Sunflower Seed Oil sebagai pelembap alami untuk merawat bibir agar tetap lembap.</p>
+      <button type="submit" class="group mt-4 mr-2 mb-4 flex cursor-pointer text-left text-xl font-semibold leading-tight text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-7 w-7 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+        </svg>
+      </button>
+    </div>
+    <div class="hidden h-72 w-1/2 flex-shrink-0 justify-center lg:flex">
+      <img class="" src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/987/2023/08/16/Azarine-1782110314.jpeg" alt="" />
+    </div>
+  </div>
+</div>
+<br>
+<div class="mx-auto my-5 w-screen max-w-screen-xl px-4">
+  <div class="flex rounded-xl bg-white py-2 px-2 text-gray-700 shadow-lg shadow-pink-200">
+    <div class="my-auto mr-4 p-3 text-black-500">
+      <h1 class="text-2xl font-bold">Skincare</h1>
+      <p class="mt-4 text-lg"> Serum untuk mencerahkan wajah dari bekas jerawat dan mengontrol sebum berlebih dengan teknologi nano capsulated 5% agar mempercepat proses penetrasi ke kulit dan memberikan hasil lebih cepat.</p>
+      <button type="submit" class="group mt-4 mr-2 mb-4 flex cursor-pointer text-left text-xl font-semibold leading-tight text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-7 w-7 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+        </svg>
+      </button>
+    </div>
+    <div class="hidden h-72 w-1/2 flex-shrink-0 justify-center lg:flex">
+      <img class="" src="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/7/14/c130c826-dcfb-4e61-ae93-db5b4c4b7d35.jpg" alt="" />
+    </div>
+  </div>
+</div>
+
+<section class="py-20">
+  <h1 class="mb-12 text-center font-sans text-5xl font-bold"></h1>
+  <div class="mx-auto grid max-w-screen-lg grid-cols-1 gap-5 p-5 sm:grid-cols-2 md:grid-cols-3 lg:gap-10">
+    
+    <article class="h-90 col-span-1 m-auto min-h-full cursor-pointer overflow-hidden rounded-lg pb-2 shadow-lg transition-transform duration-200 hover:translate-y-2">
+      <a href="#" class="block h-full w-full">
+        <img class="max-h-40 w-full object-cover" alt="featured image" src="https://img.ws.mms.shopee.co.id/769bda580f1682920729ee5d89eb9a43" />
+        <div class="w-full bg-white p-4">
+          <p class="mb-2 text-xl font-medium text-gray-800">Sunscreen</p>
+          <p class="text-md font-light text-gray-400">Sunscreen atau juga dikenal dengan tabir surya adalah produk perawatan kulit berfungsi untuk melindungi kulit dari pengaruh sinar UV dari matahari yang dapat merusak kulit.</p>
+        </div>
+      </a>
+    </article>
+    
+    <article class="h-90 col-span-1 m-auto min-h-full cursor-pointer overflow-hidden rounded-lg pb-2 shadow-lg transition-transform duration-200 hover:translate-y-2">
+      <a href="#" class="block h-full w-full">
+        <img class="max-h-40 w-full object-cover" alt="featured image" src="https://down-id.img.susercontent.com/file/sg-11134201-22110-ckgvlonbq3jv6b" />
+        <div class="w-full bg-white p-4">
+          <p class="mb-2 text-xl font-medium text-gray-800">Moisturizer</p>
+          <p class="text-md font-light text-gray-400">Moisturizer adalah produk yang dirancang untuk menghidrasi den melembapkan kulit dengan cara meningkatkan kadar air.</p>
+        </div>
+      </a>
+    </article>
+    
+    <article class="h-90 col-span-1 m-auto min-h-full cursor-pointer overflow-hidden rounded-lg pb-2 shadow-lg transition-transform duration-200 hover:translate-y-2">
+      <a href="#" class="block h-full w-full">
+        <img class="max-h-40 w-full object-cover" alt="featured image" src="https://asset-2.tstatic.net/shopping/foto/bank/images/azarine-active-bright-brightening-creamy-facial-foam-50-gram-q24.jpg" />
+        <div class="w-full bg-white p-4">
+          <p class="mb-2 text-xl font-medium text-gray-800">Facial Wash</p>
+          <p class="text-md font-light text-gray-400"> Face wash merupakan sabun khusus yang digunakan untuk membersihkan wajah. Fungsi face wash adalah mengangkat kotoran dan minyak yang menempel di kulit wajah. Produk ini memiliki konsistensi krim, gel, hingga cairan atau busa yang cukup lembut</p>
+        </div>
+      </a>
+    </article>
+  </div>
+</section>
+<section class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="mx-auto max-w-screen-xl px-4 md:px-8"> 
+    <!-- Heading -->
+    <div class="mb-10 md:mb-16">
+      <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Azarine Paket Series</h2>
+
+      <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">Temukan berbagai produk kecantikan dan perawatan kulit terbaru yang dapat mengatasi berbagai macam masalah kulit Anda dari Paket Azarine Cosmetic.</p>
+    </div>
+    <!-- /Heading -->
+    <div class="grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-2 xl:grid-cols-2 xl:gap-16">
+      <!-- Article -->
+      <article class="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
+        <a href="#" class="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
+          <img src="https://images.ctfassets.net/qqbjnb77o4id/iD4rElDulzOtvZwMdAA1Y/739ebdf60a50a317d8b28c7857171fed/C_Series-1-3__1_.jpg?w=780&fm=webp" loading="lazy" alt="" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2">
+          <h2 class="text-xl font-bold text-gray-800">
+            <a href="#" class="transition duration-100 hover:text-rose-500 active:text-rose-600">Paket C White Series </a>
+          </h2>
+
+          <p class="text-gray-500">Rangkaian Azarine C White Series ini dapat mengatasi kulit kusam, berminyak dan pori-pori besar.</p>
+
+          <div>
+            <a href="#" class="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
+          </div>
+        </div>
+      </article>
+      <!-- /Article -->
+      <!-- Article -->
+      <article class="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
+        <a href="#" class="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
+          <img src="https://diary.marshabeauty.com/media/article_images/1775/Azarine-Acne-Blemish-Rescue.jpg" loading="lazy" alt="" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2">
+
+          <h2 class="text-xl font-bold text-gray-800">
+            <a href="#" class="transition duration-100 hover:text-rose-500 active:text-rose-600">Acne Series</a>
+          </h2>
+
+          <p class="text-gray-500">Azarine Acne  mengandung 2% Succinic Acid, Green Tea, dan Cinnamon Bark yang mampu mengontrol sebum dan bertindak sebagai antibakteri. Produk ini juga mengandung 2% Arbutin yang bermanfaat untuk membantu mencerahkan bekas jerawat.</p>
+
+          <div>
+            <a href="#" class="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
+          </div>
+        </div>
+      </article>
+      <!-- /Article -->
+      <!-- Article -->
+      <article class="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
+        <a href="#" class="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
+          <img src="../assets/2.jpg" loading="lazy" alt="" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2">
+
+          <h2 class="text-xl font-bold text-gray-800">
+            <a href="#" class="transition duration-100 hover:text-rose-500 active:text-rose-600">Tosca Series</a>
+          </h2>
+
+          <p class="text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint necessitatibus molestias explicabo.</p>
+
+          <div>
+            <a href="#" class="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
+          </div>
+        </div>
+      </article>
+      <!-- /Article -->
+      <!-- Article -->
+      <article class="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
+        <a href="#" class="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
+          <img src="../assets/1.jpg" loading="lazy" alt="" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+        </a>
+
+        <div class="flex flex-col gap-2">
+
+          <h2 class="text-xl font-bold text-gray-800">
+            <a href="#" class="transition duration-100 hover:text-rose-500 active:text-rose-600">Pink Series</a>
+          </h2>
+
+          <p class="text-gray-500">Azarine Active Bright merupakan koleksi produk terbaru Azarine untuk membantu mengatasi masalah kulit kusam.</p>
+
+          <div>
+            <a href="#" class="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
+          </div>
+        </div>
+      </article>
+      <!-- /Article -->
+    </div>
+  </div>
+</section>
+
+<footer class="relative mt-20 bg-gray-900 px-4 pt-20">
+  <div class="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-xl border-4 border-sky-500 bg-white p-2"><img class="h-full object-contain" src="https://i.pinimg.com/236x/f9/55/4d/f9554daa2ab82852e5a4981da916e17a.jpg" alt="" /></div>
+  <nav aria-label="Footer Navigation" class="mx-auto mb-10 flex max-w-lg flex-col gap-10 text-center sm:flex-row sm:text-left">
+    <a href="#" class="font-medium text-white">CUSTOMER SERVICE</a>
+    <a href="#" class="font-medium text-white">SUPPORT</a>
+    <a href="#" class="font-medium text-white">BUSINESS</a>
+    <a href="#" class="font-medium text-white">ABOUT US</a>
+  </nav>
+  <p class="py-10 text-center text-gray-300">© 2023, MOoi'Colect Powered by Shopify</p>
+</footer>
+
+
+</template>
